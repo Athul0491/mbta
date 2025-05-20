3a81c4d926d18725c80521d209b8da46374fe6c9
Athul0491 <athulrthulasidasan@gmail.com>
2025-05-20 10:44:56 -0400

    extracted data from the API to CSV

---
+++ mbta.py
@@ -0,0 +1,8 @@
+# MBTA api constants
+
+base_url = 'https://api-v3.mbta.com/'
+
+routes_url = base_url + 'routes'
+stops_url = base_url + 'stops'
+lines_url = base_url + 'lines'
+vehicles_url = base_url + 'vehicles'
